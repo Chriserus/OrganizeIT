1356be886070694d4b3d0548cde4f6b1b20b57a6
Chriserus <krzysborowiecki97@gmail.com>
2019-07-22 19:35:49 +0200

    Initial commit

---
+++ src/main/java/com/capgemini/organizeIT/OrganizeItApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.organizeIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizeItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrganizeItApplication.class, args);
+    }
+
+}
