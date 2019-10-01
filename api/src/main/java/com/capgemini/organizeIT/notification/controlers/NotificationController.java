87b82b0e1b1fb29ab5adc922468912c6bf7046d0
Chriserus <krzysborowiecki97@gmail.com>
2019-10-01 19:51:45 +0200

    Registering token to topic and user topics backend

--- api/src/main/java/com/capgemini/organizeIT/notification/controlers/NotificationController.java
+++ api/src/main/java/com/capgemini/organizeIT/notification/controlers/NotificationController.java
@@ -1,6 +1,9 @@
 package com.capgemini.organizeIT.notification.controlers;
 
+import com.capgemini.organizeIT.permission.entities.Permission;
 import com.capgemini.organizeIT.permission.services.PermissionService;
+import com.capgemini.organizeIT.user.entities.User;
+import com.capgemini.organizeIT.user.services.UserService;
 import lombok.extern.log4j.Log4j2;
 import org.json.JSONObject;
 import org.springframework.web.bind.annotation.*;
@@ -17,38 +20,70 @@ import java.util.Map;
 @RestController
 @CrossOrigin
 public class NotificationController {
+    private static final String AUTHENTICATION_KEY = "key=";
+    private static final String GOOGLE_API_URL = "https://fcm.googleapis.com/fcm/send";
+    private static final String TOPIC_REGISTRATION_URL = "https://iid.googleapis.com/iid/v1:batchAdd";
+    private static final String APPLICATION_JSON = "application/json";
     private final PermissionService permissionService;
+    private final UserService userService;
 
-    public NotificationController(final PermissionService permissionService) {
+    public NotificationController(final PermissionService permissionService, final UserService userService) {
         this.permissionService = permissionService;
+        this.userService = userService;
     }
 
-    // TODO: Post request - adds received body to "to" json
+    @PostMapping(value = "/api/permissions/{userEmail}/", consumes = "application/json", produces = "application/json")
+    public Permission register(@RequestBody Permission permission, @PathVariable String userEmail) {
+        User user = userService.findByEmail(userEmail);
+        permission.setHolder(user);
+        registerTokenToTopic(permission.getToken(), user.getEmail());
+        log.info(permission);
+        return permissionService.save(permission);
+    }
+
     @PostMapping(value = "/api/notification/{userEmail}", consumes = "application/json", produces = "application/json")
     public void sendNotificationToUser(@RequestBody Map<String, String> notificationJson, @PathVariable String userEmail) {
-        permissionService.findByEmail(userEmail).forEach(permission -> {
-            log.info(permission.getToken());
-            try {
-                sendNotificationWithBodyToToken(notificationJson, permission.getToken());
-            } catch (URISyntaxException | IOException | InterruptedException e) {
-                log.error(e);
-            }
-        });
+        sendNotificationWithBodyToRecipient(notificationJson, "/topics/" + userEmail);
     }
 
-    private void sendNotificationWithBodyToToken(Map<String, String> body, String token) throws URISyntaxException, IOException, InterruptedException {
+    /**
+     * Method used to send notification to certain token or topic passed as recipient parameter
+     *
+     * @param body      json notification body
+     * @param recipient token or certain topic to send notification to
+     */
+    private void sendNotificationWithBodyToRecipient(Map<String, String> body, String recipient) {
         JSONObject requestJson = new JSONObject();
         requestJson.put("notification", body);
-        requestJson.put("to", token);
+        requestJson.put("to", recipient);
+        try {
+            postRequestToUrlWithAuthorization(requestJson, GOOGLE_API_URL);
+        } catch (URISyntaxException | IOException | InterruptedException e) {
+            log.error(e);
+        }
+    }
+
+    private void postRequestToUrlWithAuthorization(JSONObject requestJson, String url) throws URISyntaxException, IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(new URI("https://fcm.googleapis.com/fcm/send"))
+                .uri(new URI(url))
                 // TODO: Store key in some secret place
-                .header("Authorization", "key=")
-                .header("Content-Type", "application/json")
+                .header("Authorization", AUTHENTICATION_KEY)
+                .header("Content-Type", APPLICATION_JSON)
                 .POST(HttpRequest.BodyPublishers.ofString(requestJson.toString()))
                 .build();
-
         HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
         log.info(response.body());
+    }
+
+
+    private void registerTokenToTopic(String token, String userEmail) {
+        JSONObject requestJson = new JSONObject();
+        requestJson.put("to", "/topics/" + userEmail);
+        requestJson.put("registration_tokens", token);
+        try {
+            postRequestToUrlWithAuthorization(requestJson, TOPIC_REGISTRATION_URL);
+        } catch (URISyntaxException | IOException | InterruptedException e) {
+            log.error(e);
+        }
     }
 }
