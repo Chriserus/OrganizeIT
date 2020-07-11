534cc566b6b49a84e7b57362665a7fd11d326a5e
Chriserus <krzysborowiecki97@gmail.com>
2020-07-11 19:52:02 +0200

    Basic Swagger introduction

--- api/src/main/java/com/capgemini/organizeIT/api/configuration/SecurityConfiguration.java
+++ api/src/main/java/com/capgemini/organizeIT/api/configuration/SecurityConfiguration.java
@@ -56,7 +56,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/shirt-sizes").permitAll()
                 .antMatchers("/api/user").permitAll()
                 .antMatchers("/api/users").hasRole("ADMIN")
-                .antMatchers(HttpMethod.DELETE,"/api/users/{userId}").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/api/users/{userId}").hasRole("ADMIN")
                 .antMatchers(HttpMethod.PUT, "/api/users/{userId}").authenticated()
                 .antMatchers("/api/users/emails/{email}/").permitAll()
                 .antMatchers("/api/users/{userId}/projects").authenticated()
--- core/src/main/java/com/capgemini/organizeIT/core/project/model/ProjectDto.java
+++ core/src/main/java/com/capgemini/organizeIT/core/project/model/ProjectDto.java
@@ -3,6 +3,7 @@ package com.capgemini.organizeIT.core.project.model;
 import com.capgemini.organizeIT.infrastructure.user.entities.City;
 import lombok.Data;
 
+import javax.validation.constraints.Min;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
@@ -15,6 +16,7 @@ public class ProjectDto {
     private String technologies;
     private Set<MembershipDto> members = new HashSet<>();
     private Set<OwnershipDto> owners = new HashSet<>();
+    @Min(value = 1, message = "Project should have at lease one member slot")
     private Integer maxMembers = 1;
     private boolean verified = false;
     private boolean confirmed = false;
