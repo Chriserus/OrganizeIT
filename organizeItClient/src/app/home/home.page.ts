e040b3b898bc3a0dc710d208061ff186d9588ba8
Krzysztof Borowiecki <krzysborowiecki97@gmail.com>
2021-08-09 20:44:05 +0200

    Show latest announcements on Home Page

--- organizeItClient/src/app/home/home.page.ts
+++ organizeItClient/src/app/home/home.page.ts
@@ -1,9 +1,11 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ProjectService} from '../project/project.service';
 import {Project} from '../interfaces/project.model';
+import {Comment} from '../interfaces/comment.model';
 import {Subject} from 'rxjs';
 import {finalize, takeUntil} from 'rxjs/operators';
 import {DataService} from "../shared/data.service";
+import {CommentService} from "../comment/comment.service";
 
 @Component({
     selector: 'app-home',
@@ -14,6 +16,7 @@ export class HomePage implements OnInit, OnDestroy {
     projects: Project[] = [];
     unsubscribe: Subject<Project[]> = new Subject();
     showProjectsSpinner: boolean;
+    announcements: Comment[] = [];
     slideOptsOne = {
         initialSlide: 0,
         slidesPerView: 1,
@@ -23,17 +26,20 @@ export class HomePage implements OnInit, OnDestroy {
         }
     };
 
-    constructor(public projectService: ProjectService, private data: DataService) {
+    constructor(public projectService: ProjectService, public commentService: CommentService, private data: DataService) {
         this.getProjects();
+        this.getAnnouncements();
         this.data.currentProjects.subscribe(projects => {
             this.projects = projects.filter(project => project.verified);
         });
+        this.data.currentComments.subscribe(comments => this.announcements = comments.filter(comment => comment.announcement));
     }
 
     ngOnInit() {
         this.data.currentProjects.subscribe(projects => {
             this.projects = projects.filter(project => project.verified);
         });
+        this.data.currentComments.subscribe(comments => this.announcements = comments.filter(comment => comment.announcement));
     }
 
     ngOnDestroy() {
@@ -50,6 +56,14 @@ export class HomePage implements OnInit, OnDestroy {
             .subscribe(projects => {
                 //TODO: Filter projects on backend, make an endpoint that uses clustered index
                 this.data.changeProjects(projects);
+            });
+    }
+
+    getAnnouncements() {
+        this.commentService.getComments().pipe(takeUntil(this.unsubscribe))
+            .subscribe(comments => {
+                //TODO: Filter comments on backend, make an endpoint that uses clustered index
+                this.data.changeComments(comments);
             });
     }
 }
